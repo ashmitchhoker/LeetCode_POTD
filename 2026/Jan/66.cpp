44a9285a69bf514f0e0ab940c4f0d905d7c60194
Ashmit Chhoker <ashmitchhoker64@gmail.com>
2026-01-03 16:18:23 +0530

    Plus One

---
+++ 2026/Jan/66.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> plusOne(vector<int> & digits){
+    int carry = 1;
+
+    for (int i = digits.size() - 1 ; i >=0; i--)
+    {
+        int sum = digits[i] + carry;
+        digits[i] = sum % 10;
+        carry = sum / 10;
+    }
+    
+
+    if (carry)
+    {
+        digits.insert(digits.begin() , 1);
+    }
+    return digits;
+}
+
+int main(){
+    vector<int> digits = {9};
+
+    vector<int> result =  plusOne(digits);
+
+    for (int i = 0; i < result.size(); i++)
+    {
+        cout << result[i];
+    }
+    return 0;
+}
