b73e0bb163dc8f9475678aa5266643a6b1c17fd2
Ashmit Chhoker <ashmitchhoker64@gmail.com>
2026-08-04 01:13:24 +0530

    Stone Game

---
+++ 2026/Aug/877.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+bool stoneGame(vector<int>& piles){
+    return 1;
+}
+
+
+int main(){
+    vector<int> nums = {5 , 3 , 4 , 5};
+
+    cout << stoneGame(nums);
+    return 0;
+}
